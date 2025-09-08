26a660e37f5c83ccc87e512dd82f668cd2491740
Nameera khan <nameerakhan54@icloud.com>
2025-09-08 20:17:39 +0530

    deploy 1

--- model/Metadata.js
+++ model/Metadata.js
@@ -1,11 +1,4 @@
-// // model/MetaData.js
-// const mongoose = require('mongoose');
 
-// const metaDataSchema = new mongoose.Schema({}, { strict: false, timestamps: true });
-
-// module.exports = mongoose.model('MetaData', metaDataSchema);
-
-// model/MetaData.js
 const mongoose = require('mongoose');
 
 const metaDataSchema = new mongoose.Schema({}, { strict: false, timestamps: true });
